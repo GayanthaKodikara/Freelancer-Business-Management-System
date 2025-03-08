326e40fa03f9f78875ad6d8f6b97238862fc969c
GayanthaKodikara <kodikaragayantha@gmail.com>
2025-03-08 16:09:42 +0530

    get all employee

--- employee_management.py
+++ employee_management.py
@@ -13,8 +13,47 @@ def get_employees():
     #print(results) # print in console
     users = [] # initial blank list
     for row in results: #for loop
-        users.append({'id': row[0], 'name': row[1], 'email': row[2]})
+        users.append({'emp_id': row[0], 'first_name': row[1], 'last_name': row[2], 'email':row[9], 'address':row[3],
+        'nic': row[4],'birth_day': row[5],'role': row[6],'workshop_name': row[7],'design_category': row[8],})
     #print(users) # print in console
     return jsonify(users)
     #return jsonify(results) 
     #return str(results)  #for string result
+
+
+@app.route('/add_employee', methods=['POST'])
+def add_employee():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    data = request.get_json()
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    email = data.get('email')
+    address = data.get('address')
+    nic = data.get('nic')
+    birth_day = data.get('birth_day')
+    role = data.get('role')
+    workshop_name = data.get('workshop_name')
+    design_category = data.get('design_category')
+
+    print(data)
+
+    if not first_name or not email:
+        return jsonify({'error': 'Name and email are required'}), 400
+        
+
+    try:
+        cursor.execute("INSERT INTO employee (first_name, last_name, email, address, nic, birth_day, role, workshop_name, design_category) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+         (first_name, last_name, email, address, nic, birth_day, role, workshop_name, design_category))
+        connection.commit()
+        cursor.close()
+        connection.close() # Close the connection
+        return jsonify({'message': 'Employee added successfully'}), 201
+    except Exception as e:
+        connection.rollback()
+        cursor.close()
+        connection.close() # Close the connection
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
